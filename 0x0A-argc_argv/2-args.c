477a646d19693dd72087d9e13e5bedf7fe6adb25
malit mohamud <malitmohamud@gmail.com>
2023-03-10 08:47:37 -0800

    pushed now

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 		
 #include "main.h"
@@ -7,7 +6,7 @@
 		
 /**
 		
- * main - print the number of arguments passed to the program
+ * main - prints all arguments it receives
 		
  * @argc: number of arguments
 		
@@ -19,15 +18,21 @@
 		
  */
 		
-
-		
 int main(int argc, char *argv[])
 		
 {
 		
-(void) argv; /*Ignore argv*/
+	int i;
 		
-	printf("%d\n", argc - 1);
+
+		
+	for (i = 0; i < argc; i++)
+		
+	{
+		
+		printf("%s\n", argv[i]);
+		
+	}
 		
 
 		
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,11 +1,1 @@
-. C - argc, argv
-C - argc, argv
-		
-
-		
-TASKS
-		
-
-		
-[0. It ain't what they call you, it's what you answer to](0-whatsmyname.c)
-
+0x0A. C - argc, argv
